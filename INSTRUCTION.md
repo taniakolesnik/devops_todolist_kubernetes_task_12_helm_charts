b77d46326af535c93a50f6203fa0487b9a4c272f
Tania Kolesnik <kolesnik@gmail.com>
2025-12-31 19:29:57 +0000

    fixed location

--- INSTRUCTION.md
+++ INSTRUCTION.md
@@ -1,4 +1,4 @@
 kubectl get nodes --show-labels
 kubectl taint node kind-worker kind-worker2 app=mysql:NoSchedule
-helm install todoapp-release ./helm-chart/todoapp/
+helm install todoapp-release .infrastructure/helm-chart/todoapp
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 kubectl taint node kind-worker kind-worker2 app=mysql:NoSchedule
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-
+helm install todoapp-release .infrastructure/helm-chart/todoapp
 
 
 # #!/bin/bash
